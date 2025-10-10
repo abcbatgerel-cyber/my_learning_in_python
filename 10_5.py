31aed3569187ff037ebab84e2970e7defc8693aa
Batgerel <BatgerelBatgerel575@gmail.com>
2025-10-10 22:59:13 +0800

    10 sariin 10-nii odor hackerrank-iin 2 bodlogiig nemj mon code-oo iluu tsegtstei barilald oruullaa

--- 10_5.py
+++ 10_5.py
@@ -1,5 +1,91 @@
-# 1 
+# 1
 
+# "solve me first" in hackerrank's python
+
+# def solveMeFirst(a,b):
+# 	return a + b
+
+# num1 = int(input())
+# num2 = int(input())
+# res = solveMeFirst(num1,num2)
+# print(res)
+
+
+
+
+
+# 2
+# simple array sum in hackerrank's python
+# def simpleArraySum(ar):
+#     # Write your code here
+#     total_sum = sum(ar)
+#     return total_sum
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     ar_count = int(input().strip())
+
+#     ar = list(map(int, input().rstrip().split()))
+
+#     result = simpleArraySum(ar)
+
+#     fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+
+
+
+# 3
+
+# compare triplets in hackerrank's python
+# def compareTriplets(a, b):
+
+#     # Write your code here
+#     a_score = 0
+#     b_score = 0
+    
+#     for i in range(3):
+#         if a[i] > b[i]:
+#             a_score += 1
+#         elif a[i] < b[i]:
+#             b_score += 1
+#     return (a_score, b_score)
+
+
+
+
+
+
+# 4
+# very big sum in hackerrank's python
+
+# def aVeryBigSum(ar):
+#     # Write your code here
+#     long = sum(ar)
+#     return long
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     ar_count = int(input().strip())
+
+#     ar = list(map(int, input().rstrip().split()))
+
+#     result = aVeryBigSum(ar)
+
+#     fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+
+
+
+
+# 5
 # solve diagnol difference in hackerrank's python
 
 # def diagonalDifference(arr):
@@ -33,105 +119,66 @@
 
 
 
+# 6
+# plus minus in hackerrank
 
-
-
-
-
-# 2 
-
-# "solve me first" in hackerrank's python
-
-# def solveMeFirst(a,b):
-# 	return a + b
-
-
-# num1 = int(input())
-# num2 = int(input())
-# res = solveMeFirst(num1,num2)
-# print(res)
-
-
-
-
-
-
-
-
-
-# 3 
-
-# simple array sum in hackerrank's python
-# def simpleArraySum(ar):
+# def plusMinus(arr):
 #     # Write your code here
-#     total_sum = sum(ar)
-#     return total_sum
+#     num = len(arr)
+#     positive = 0
+#     negative = 0
+#     zero = 0
+#     for num in arr:
+#         if num > 0:
+#             positive += 1
+#         elif num < 0:
+#             negative += 1
+#         else:
+#             zero += 1
+        
+#     print(f"{positive/n:.6f}")
+#     print(f"{negative/n:.6f}")
+#     print(f"{zero/n:.6f}")
+
 
 # if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     n = int(input().strip())
 
-#     ar_count = int(input().strip())
+#     arr = list(map(int, input().rstrip().split()))
 
-#     ar = list(map(int, input().rstrip().split()))
-
-#     result = simpleArraySum(ar)
-
-#     fptr.write(str(result) + '\n')
-
-#     fptr.close()
+#     plusMinus(arr)
 
 
 
 
 
 
-
-
-
-
-# 4 
-
-# compare triplets in hackerrank's python
-# def compareTriplets(a, b):
-
-#     # Write your code here
-#     a_score = 0
-#     b_score = 0
+# 7
+# timeConversion in hackerrank
+# def timeConversion(s):
+#     # Цаг, минут, секунд, AM/PM салгах
+#     hour = int(s[:2])
+#     minute = int(s[3:5])
+#     second = int(s[6:8])
+#     period = s[8:]  # "AM" эсвэл "PM"
     
-#     for i in range(3):
-#         if a[i] > b[i]:
-#             a_score += 1
-#         elif a[i] < b[i]:
-#             b_score += 1
-#     return (a_score, b_score)
-
-
-
-
-
-
-
-
-
-# 5 
-
-# very big sum in hackerrank's python
-
-# def aVeryBigSum(ar):
-#     # Write your code here
-#     long = sum(ar)
-#     return long
+#     if period == "AM":
+#         if hour == 12:
+#             hour = 0
+#     else:  # PM
+#         if hour != 12:
+#             hour += 12
+    
+#     return "{:02}:{:02}:{:02}".format(hour, minute, second)
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-#     ar_count = int(input().strip())
+#     s = input()
 
-#     ar = list(map(int, input().rstrip().split()))
+#     result = timeConversion(s)
 
-#     result = aVeryBigSum(ar)
-
-#     fptr.write(str(result) + '\n')
+#     fptr.write(result + '\n')
 
 #     fptr.close()
 
@@ -143,8 +190,7 @@
 
 
 
-
-# # 6 
+# 8 
 # 
 # mini max sum
 # def miniMaxSum(arr):
@@ -168,7 +214,7 @@
 
 
 
-# 7 
+# 9
 # birthdayCakeCandles in hackerrank's python
 
 # def birthdayCakeCandles(candles):
