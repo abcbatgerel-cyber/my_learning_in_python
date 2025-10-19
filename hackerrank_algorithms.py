c6f57e7d6fda9f08118e48cd4dbf8337ba5bad8d
Batgerel <BatgerelBatgerel575@gmail.com>
2025-10-19 12:23:20 +0800

    10 sariin 19-nd

--- hackerrank_algorithms.py
+++ hackerrank_algorithms.py
@@ -269,27 +269,19 @@
 
 
 # 11
-# import os
-# import sys
 
 # def timeConversion(s):
-#     period = s[-2:]          # AM or PM
-#     time = s[:-2]            # HH:MM:SS part
-#     hour, minute, second = time.split(':')
-
+#     period = s[-2]
+#     time = s[:-2]
+#     hour, minute, secund = time.split(":")
 #     if period == "AM":
 #         if hour == "12":
 #             hour = "00"
-#     else:  # PM
+#     else:
 #         if hour != "12":
 #             hour = str(int(hour) + 12)
+#     return f"{hour}:{minute}:{secund}"
 
-#     return f"{hour}:{minute}:{second}"
-
-
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-#     s = input().strip()
-#     result = timeConversion(s)
-#     fptr.write(result + '\n')
-#     fptr.close()
+# s = input("tsagaa bicheerei tsagaa bichihdee am or pm gedgee zo bivheerei: ")
+# result = timeConversion(s)
+# print(result)
